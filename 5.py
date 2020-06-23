ab1ff8371a932f0877c591f5ac9a510d999d1361
jackpalaia <jack@jackpalaia.com>
2020-06-23 13:01:56 -0400

    finished

--- 5.py
+++ 5.py
@@ -1,13 +1,16 @@
 def prime_factors(n):
   factors = []
   temp = n
+  x = 2
 
-  for x in range(2, n):
+  while x < n:
     if (temp % x == 0):
       factors.append(x)
       temp /= x
-    if (temp == 1):
-      break
+      if (x == 1):
+        break
+      continue
+    x += 1
 
   if (len(factors) == 0):
     return [n]
@@ -15,11 +18,31 @@ def prime_factors(n):
   return factors
 
 def divisible():
-  
+  factorDict = {}
+  for x in range(1, 21):
+    tempDict = {}
+    factors = prime_factors(x)
+    for j in factors:
+      if j not in tempDict.keys():
+        tempDict[j] = 1
+      else:
+        tempDict[j] += 1
+    for k in tempDict.keys():
+      if k not in factorDict.keys():
+        factorDict[k] = tempDict[k]
+      else:
+        if tempDict[k] > factorDict[k]:
+          factorDict[k] = tempDict[k]
+  print(factorDict)
+  product = 1
+  for k, v in factorDict.items():
+    for i in range(v):
+      product *= k
+  return product
 
 
 def main():
-  print(prime_factors(21))
+  print(divisible())
 
 if __name__ == "__main__":
   main()
